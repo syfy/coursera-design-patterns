db102a44544f53a485d591e046827cc829eb83a3
randomblitz9 <randomblitz9@gmail.com>
2020-06-26 21:44:47 +0800

    initial Commit

---
+++ src/Playlist.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Playlist implements IComponent {
+
+	public String playlistName;
+	public ArrayList<IComponent> playlist = new ArrayList();
+
+	public Playlist(String playlistName) {
+		this.playlistName = playlistName;
+	}
+
+	// Your code goes here!
+
+	public void play() {
+		System.out.println("playing Playlist");
+		for (IComponent t : playlist) {
+			t.play();
+		}
+
+	};
+
+	public void setPlaybackSpeed(float speed) {
+		System.out.println("adjusting speed for entier playlist");
+		for (IComponent t : playlist) {
+			t.setPlaybackSpeed(speed);
+		}
+	};
+
+	String getName() {
+		return playlistName;
+
+	}
+
+	void add(IComponent component) {
+		playlist.add(component);
+	}
+
+	void remove(IComponent component) {
+		playlist.remove(component);
+
+	}
+
+}
---
+++ src/Song.java
@@ -0,0 +1,32 @@
+
+public class Song implements IComponent {
+	public String songName;
+	public String artist;
+	public float speed = 1; // Default playback speed
+
+	public Song(String songName, String artist) {
+		this.songName = songName;
+		this.artist = artist;
+	}
+
+	// Your code goes here!
+
+	public void play() {
+		// play
+		System.out.println(songName+"is Playing at "+speed+"bpm speed");
+	};
+
+	public void setPlaybackSpeed(float speed) {
+		System.out.println("setting playBackSpeed to "+speed);
+		this.speed = speed;
+	};
+
+	String getName() {
+		return songName;
+	}
+
+	String getArtist() {
+		return this.artist;
+	}
+
+}
